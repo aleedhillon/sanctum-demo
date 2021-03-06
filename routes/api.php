a3002c3762b45b9b6c6241456138b61ffd7b2529
Ali A. Dhillon <aleedhx@gmail.com>
2021-03-06 16:52:59 +0500

    cretaes routes and changes in User model

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\AuthUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', AuthUserController::class);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
